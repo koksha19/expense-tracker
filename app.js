d8cca484cb81d81edd049a7956d5ebb622227364
Lev <lev.bereza@gmail.com>
2024-05-14 20:13:12 +0300

    Updating balance

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const state = {
-    balance: 4800,
-    income: 5000,
-    expense: 200,
+    balance: null,
+    income: null,
+    expense: null,
     transactions: [
-        { type: 'income', category: 'salary', sum: 5000, date: '14-05-2024' },
-        { type: 'expense', category: 'groceries', sum: 200, date: '16-05-2024' },
+        { date: '14-05-2024', type: 'income', category: 'salary', sum: 5000 },
+        { date: '16-05-2024', type: 'expense', category: 'groceries', sum: 200 },
+        { date: '17-05-2024', type: 'expense', category: 'rent', sum: 1100 },
+        { date: '18-05-2024', type: 'income', category: 'salary', sum: 5000 },
     ],
 };
 
@@ -15,44 +17,69 @@ const income = document.querySelector('#income');
 const expense = document.querySelector('#expense');
 const transactionsTable = document.querySelector('.transactions');
 
+const loadPage = () => {
+    updateBalance();
+    render();
+}
+
 const render = () => {
     balance.innerHTML = `$${state.balance}`;
     income.innerHTML = `$${state.income}`;
     expense.innerHTML = `$${state.expense}`;
 
     const transactions = state.transactions.length;
-
     for(let i = 0; i < transactions; i++) {
         const transactionElement = document.createElement('tr');
+        const date = document.createElement('td');
         const type = document.createElement('td');
         const category = document.createElement('td');
         const sum = document.createElement('td');
-        const date = document.createElement('td');
         const deleteButton = document.createElement('td');
         const btn = document.createElement('button');
 
-        const transactionToAppend = state.transactions[i];
+        const transaction = state.transactions[i];
 
-        type.append(transactionToAppend.type);
+        date.append(transaction.date);
+        transactionElement.appendChild(date);
+
+        type.append(transaction.type);
         transactionElement.appendChild(type);
 
-        category.append(transactionToAppend.category);
+        category.append(transaction.category);
         transactionElement.appendChild(category);
 
-        sum.append(transactionToAppend.sum);
+        sum.append(transaction.sum);
         transactionElement.appendChild(sum);
-
-        date.append(transactionToAppend.date);
-        transactionElement.appendChild(date);
 
         btn.append('Delete');
         deleteButton.appendChild(btn);
         transactionElement.appendChild(deleteButton);
 
         transactionsTable.appendChild(transactionElement);
-        console.log(type, category, sum, date, deleteButton, btn);
     }
 }
-render();
+const updateBalance = () => {
+    let balance;
+    let income = 0;
+    let expense = 0;
+
+    const transactions = state.transactions.length;
+    for(let i = 0; i < transactions; i++) {
+        const transaction = state.transactions[i];
+        if(transaction.type === 'income') {
+            income += transaction.sum;
+        } else {
+            expense += transaction.sum;
+        }
+        console.log(income, expense);
+    }
+    balance = income - expense;
+
+    state.balance = balance;
+    state.income = income;
+    state.expense = expense;
+}
+
+loadPage();
 
 
